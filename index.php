ec0d8a74915837e640db518d7ec282e33f86f4f9
Maxime <chamalowat@laposte.net>
2017-11-06 10:19:39 +0100

    Ajout d'un menu et footer type
    Les fichiers sont vides pour le moment

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="stylesheet.css"/>
+	<title>Lyon 9</title>
+</head>
+<body>
+<?php require_once("./menu.php"); ?>
+<?php require_once("./footer.php");?>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo "<a href = './index.php'>Histoire du quartier</a>"; 
+echo "<a href = 'index.php?exercice=galleries'>Culture</a>";
+echo "<a href = 'index.php?exercice=grilles'>Sports et Loisirs</a>"
+
+?>

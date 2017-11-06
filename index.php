a10d733e96806160938fb4865e0826193e64f41a
Maxime <chamalowat@laposte.net>
2017-11-06 11:42:41 +0100

    Inclusion des polices de caractère

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
 <head>
 	<link rel="stylesheet" href="stylesheet.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Vollkorn" rel="stylesheet">
 	<title>Lyon 9</title>
 </head>
 <body>
 <?php require_once("./menu.php"); ?>
+<p>Ceci est un paragraphe</p>
 <?php require_once("./footer.php");?>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php 
 
-echo "<a href = './index.php'>Histoire du quartier</a>"; 
-echo "<a href = 'index.php?exercice=galleries'>Culture</a>";
-echo "<a href = 'index.php?exercice=grilles'>Sports et Loisirs</a>"
+echo "<a class = 'menu_link' href = './index.php'>Histoire du quartier</a>"; 
+echo "<a class = 'menu_link' href = 'index.php?exercice=galleries'>Culture</a>";
+echo "<a class = 'menu_link' href = 'index.php?exercice=grilles'>Sports et Loisirs</a>"
 
 ?>
